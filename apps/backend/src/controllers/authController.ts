09d0382c2c28e7b857f991f4d4be5114b2fc457b
ProgrammingInspire <gurukarthi1011@gmail.com>
2026-05-23 18:54:19 +0530

    add user register api with mongodb

---
+++ apps/backend/src/controllers/authController.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express";
+import User from "../models/User";
+
+export const registerUser = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    res.status(201).json({
+      message: "User registered successfully",
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server Error",
+      error,
+    });
+  }
+};
