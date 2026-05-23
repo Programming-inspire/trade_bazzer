89967875978a784b96d2058986121d27bf3f5a10
ProgrammingInspire <gurukarthi1011@gmail.com>
2026-05-23 19:20:15 +0530

    add user login api

--- apps/backend/src/controllers/authController.ts
+++ apps/backend/src/controllers/authController.ts
@@ -36,3 +36,42 @@ export const registerUser = async (
     });
   }
 };
+
+
+export const loginUser = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid credentials",
+      });
+    }
+
+    const isPasswordMatch = await bcrypt.compare(
+      password,
+      user.password
+    );
+
+    if (!isPasswordMatch) {
+      return res.status(400).json({
+        message: "Invalid credentials",
+      });
+    }
+
+    res.status(200).json({
+      message: "Login successful",
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server Error",
+      error,
+    });
+  }
+};
